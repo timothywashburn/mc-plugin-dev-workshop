83483403b7b9f89ec24923f19af494eb8f822d1d
Timothy <timothyrwashburn@gmail.com>
2025-05-06 20:55:46 -0700

    initial commit

---
+++ src/main/java/dev/yourname/myparkour/MyParkour.java
@@ -0,0 +1,17 @@
+package dev.yourname.myparkour;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MyParkour extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		// Plugin startup logic
+
+	}
+
+	@Override
+	public void onDisable() {
+		// Plugin shutdown logic
+	}
+}
